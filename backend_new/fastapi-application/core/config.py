b24d9ce207d9a8e01c0a767f799b1052178449ae
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-16 01:29:41 +0300

    add .env config + env.template  + lifespan

--- backend_new/fastapi-application/core/config.py
+++ backend_new/fastapi-application/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -11,12 +11,18 @@ class ApiPrefix(BaseModel):
 
 class DataBaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    max_overflow: int = 10,
-    pool_size: int = 50,
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DataBaseConfig
--- backend_new/fastapi-application/core/models/db_helper.py
+++ backend_new/fastapi-application/core/models/db_helper.py
@@ -40,5 +40,5 @@ db_helper = DatabaseHelper(
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     max_overflow=settings.db.max_overflow,
-    pool_size=settings.db.polo_size,
+    pool_size=settings.db.pool_size,
 )
