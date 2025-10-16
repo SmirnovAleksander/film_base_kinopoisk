5136cb6eb7e249f2bfbd9dea4d8bea0beee816b1
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-16 21:53:12 +0300

    create user id type  var

--- backend_new/fastapi-application/core/models/user.py
+++ backend_new/fastapi-application/core/models/user.py
@@ -4,11 +4,12 @@ from fastapi_users.db import (SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase)
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
+from core.types.user_id import UserIdType
 
 if typing.TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     pass
 
     @classmethod
