4cd93203e08a7e1acb80e7979dada30f438b13c5
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-18 21:42:18 +0300

    add access token

--- backend_new/fastapi-application/core/models/user.py
+++ backend_new/fastapi-application/core/models/user.py
@@ -14,4 +14,4 @@ class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
