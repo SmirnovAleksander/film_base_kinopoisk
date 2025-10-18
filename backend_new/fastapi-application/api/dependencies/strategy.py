9f1e5fb0886397a2e51fc5552d46dfc5d919f980
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-18 22:09:40 +0300

    add get data base strategy

--- backend_new/fastapi-application/api/dependencies/strategy.py
+++ backend_new/fastapi-application/api/dependencies/strategy.py
@@ -1,17 +1,20 @@
 from typing import TYPE_CHECKING
 
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
 
+from api.dependencies.access_tokens import get_access_token_db
 from core.config import settings
 
 if TYPE_CHECKING:
     from core.models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+
 
 def get_database_strategy(
-    access_token_db: AccessTokenDatabase["AccessToken"] = Depends(get_access_token_db),
+    access_tokens_db: AccessTokenDatabase["AccessToken"] = Depends(get_access_token_db),
 ) -> DatabaseStrategy:
     return DatabaseStrategy(
-        access_token_db,
+        database=access_tokens_db,
         lifetime_seconds=settings.access_token.lifetime_seconds,
     )
