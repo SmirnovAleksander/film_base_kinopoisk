4cafe7cb74f76f9fe50f4b2ecdad8b3346bb3ce0
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-23 16:19:02 +0300

    add auth router

--- backend_new/fastapi-application/api/api_v1/__init__.py
+++ backend_new/fastapi-application/api/api_v1/__init__.py
@@ -1,14 +1,12 @@
 from fastapi import APIRouter
 
 from core.config import settings
-
-from .users import router as users_router
+from .auth import router as auth_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
 
 router.include_router(
-    users_router,
-    prefix=settings.api.v1.users,
+    auth_router,
 )
--- backend_new/fastapi-application/core/config.py
+++ backend_new/fastapi-application/core/config.py
@@ -8,7 +8,7 @@ class RunConfig(BaseModel):
 
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
-    users: str = "/users"
+    auth: str = "/auth"
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
