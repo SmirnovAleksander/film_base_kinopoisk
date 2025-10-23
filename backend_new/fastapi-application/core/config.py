339601e6914400f9f2ed94394429f33c1d0018e3
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-23 18:44:33 +0300

    add views for auth users and superusers

--- backend_new/fastapi-application/core/config.py
+++ backend_new/fastapi-application/core/config.py
@@ -10,6 +10,7 @@ class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
     auth: str = "/auth"
     users: str = "/users"
+    messages: str = "/messages"
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
