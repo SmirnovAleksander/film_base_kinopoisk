80b7c37171a625066cf02b363c9f0e9cbef37fec
SmirnovAleksander <73950970+SmirnovAleksander@users.noreply.github.com>
2025-10-30 15:58:58 +0300

    update models user media

--- backend_new/fastapi-application/core/models/user.py
+++ backend_new/fastapi-application/core/models/user.py
@@ -1,10 +1,12 @@
 import typing
 from typing import List, Optional
+from datetime import datetime
 
 from fastapi_users.db import SQLAlchemyBaseUserTable
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.orm import relationship, Mapped
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, DateTime
+from sqlalchemy.sql import func
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
@@ -19,6 +21,8 @@ class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     username: Mapped[str] = Column(String(100), unique=True, index=True, nullable=False)
     first_name: Mapped[Optional[str]] = Column(String(100), nullable=True)
     last_name: Mapped[Optional[str]] = Column(String(100), nullable=True)
+    created_at: Mapped[datetime] = Column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
 
     # Связи с пользовательскими взаимодействиями
     bookmarks: Mapped[List["Bookmark"]] = relationship("Bookmark", back_populates="user")
--- backend_new/fastapi-application/core/schemas/user.py
+++ backend_new/fastapi-application/core/schemas/user.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime
 from fastapi_users import schemas
 
 from core.types.user_id import UserIdType
@@ -8,6 +9,8 @@ class UserRead(schemas.BaseUser[UserIdType]):
     username: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+    created_at: datetime
+    updated_at: datetime
 
 
 class UserCreate(schemas.BaseUserCreate):
